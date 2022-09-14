f906ede36b356411c7e0d84b34f8ddda761bc697
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-09-14 14:59:14 +0530

    Banner created.

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,7 +1,22 @@
-const Home = () =>{
+import { Fragment } from "react"; // to avoid creation of extra div.
+import { styled, Box } from '@mui/material';
+// components
+import NavBar from "./NavBar";
+import Banner from "./Banner";
+
+const Component = styled(Box)`
+    padding: 10px;
+    background: #F2F2F2;
+`
+const Home = () => {
     return (
-        <div>Hello from home</div>
-    )
-}
+        <Fragment>
+            <NavBar />
+            <Component>
+                <Banner />
+            </Component>
+        </Fragment>
+    );
+};
 
 export default Home;
