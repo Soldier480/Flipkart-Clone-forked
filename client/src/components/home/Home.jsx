ddf01479c59196062f40c83c768b677f0cb46f1e
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-09-14 15:01:02 +0530

    Modify the Home.jsx file

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -3,7 +3,7 @@ import { styled, Box } from '@mui/material';
 // components
 import NavBar from "./NavBar";
 import Banner from "./Banner";
-
+//
 const Component = styled(Box)`
     padding: 10px;
     background: #F2F2F2;
