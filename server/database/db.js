f99d2f9f91718e838c526501befa38700808a2e5
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-09-15 12:48:35 +0530

    Database connected

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,11 @@
-export const Connection = () =>{
-    try{
+import mongoose from 'mongoose';
 
-    }catch(err){
+export const Connection = async () => {
+    const URL = 'mongodb+srv://kv:kv12345@ecommerce-web.onsxn4i.mongodb.net/?retryWrites=true&w=majority';
+    try {
+        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
+        console.log('Database connected successfully')
+    } catch (err) {
         console.log(`Error while connecting the database`, err.message);
     }
 }
