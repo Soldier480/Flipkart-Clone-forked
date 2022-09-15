d0eaeeeca05c50910a529b37a266b98b78baa200
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-09-15 11:54:32 +0530

    Modifications in server

--- server/index.js
+++ server/index.js
@@ -5,4 +5,4 @@ const app = express();
 
 const PORT = 8000;
 
-app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
