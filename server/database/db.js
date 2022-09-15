a2d0bfd22e15741f5b54dd765b0d13f69353af51
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-09-15 17:33:42 +0530

    insert operation of the products into the database

--- server/database/db.js
+++ server/database/db.js
@@ -1,13 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-export const Connection = async () => {
-    const URL = 'mongodb+srv://kv:kv12345@ecommerce-web.onsxn4i.mongodb.net/?retryWrites=true&w=majority';
+export const Connection = async (username, password) => {
+    const URL = `mongodb+srv://${username}:${password}@ecommerce-web.onsxn4i.mongodb.net/?retryWrites=true&w=majority`;
     try {
-        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
-        console.log('Database connected successfully')
+        await mongoose.connect(URL, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        });
+        console.log("Database connected successfully");
     } catch (err) {
         console.log(`Error while connecting the database`, err.message);
     }
-}
+};
 
 export default Connection;
---
+++ server/default.js
@@ -0,0 +1,14 @@
+import { products } from "./constants/product.js";
+import Product from "./model/product_schema.js";
+
+const DefaultData = async () =>{
+    try{
+        // pass products to the database
+        await Product.insertMany(products);
+        console.log('Data imported successfully');
+    }catch(err){
+        console.log(`Error while inserting default data`, err.message);
+    }
+}
+
+export default DefaultData;
