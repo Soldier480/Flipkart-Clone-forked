e9a4dbb981243c0e58a53339ba1bf0e45f51f845
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-09-16 17:12:13 +0530

    sign up and login form is completed with api testing

--- client/src/components/header/CustomButtons.jsx
+++ client/src/components/header/CustomButtons.jsx
@@ -1,5 +1,7 @@
 import { Box, Button, Typography, styled } from "@mui/material";
 import { ShoppingCart } from "@mui/icons-material";
+import LoginDialog from "../login/LoginDialog";
+import { useState } from "react";
 
 const Wrapper = styled(Box)`
   display: flex;
@@ -27,19 +29,26 @@ const LoginButton = styled(Button)`
   height: 32px;
 `;
 const CustomButtons = () => {
-    return (
-        <Wrapper>
-            <LoginButton variant="contained">Login</LoginButton>
-            <Typography style={{ marginTop: 3, width: 135 }}>
-                Become a Seller
-            </Typography>
-            <Typography style={{ marginTop: 3 }}>More</Typography>
-            <Container>
-                <ShoppingCart />
-                <Typography>Cart</Typography>
-            </Container>
-        </Wrapper>
-    );
+
+  const [open, setOpen] = useState(false);
+
+  const openDialog = () =>{
+    setOpen(true);
+  }
+  return (
+    <Wrapper>
+      <LoginButton variant="contained" onClick={()=>openDialog()}>Login</LoginButton>
+      <Typography style={{ marginTop: 3, width: 135 }}>
+        Become a Seller
+      </Typography>
+      <Typography style={{ marginTop: 3 }}>More</Typography>
+      <Container>
+        <ShoppingCart />
+        <Typography>Cart</Typography>
+      </Container>
+      <LoginDialog open={open} setOpen={setOpen} />
+    </Wrapper>
+  );
 };
 
 export default CustomButtons;
