e4e9a7e5bafd14b8b5673b11e55d511d4bb68dd6
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-09-18 00:00:38 +0530

    Values fetched from the database with redux

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,14 +1,29 @@
 import { Fragment } from "react"; // to avoid creation of extra div.
 import { styled, Box } from '@mui/material';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+
 // components
 import NavBar from "./NavBar";
 import Banner from "./Banner";
+import { getProducts } from "../../redux/actions/productActions";
+
 //
 const Component = styled(Box)`
     padding: 10px;
     background: #F2F2F2;
 `
 const Home = () => {
+
+    // state.getProducts -> from browser
+    const { products } = useSelector(state => state.getProducts);
+    console.log(products);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getProducts());
+    }, [dispatch])
     return (
         <Fragment>
             <NavBar />
--- server/controller/product-controller.js
+++ server/controller/product-controller.js
@@ -1,8 +1,8 @@
-import Product from "../model/product_schema"
+import Product from "../model/product_schema.js"
 
 export const getProducts = async ({ req, res }) => {
     try {
-        const products = await Product.find();
+        const products = await Product.find({});
         res.status(200).json(products);
     } catch (err) {
         res.status(500).json({ message: err.message });
