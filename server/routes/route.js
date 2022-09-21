a8456321081361b2b959804820a0a03ad8ff0410
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-09-21 17:19:07 +0530

    configured paytm properties in dot env file

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { addPaymentGateway } from '../controller/PaymentController.js';
 import { getProductById, getProducts } from '../controller/product-controller.js';
 import { userSignup, userlogin } from '../controller/user-controller.js';
 
@@ -9,4 +10,7 @@ router.post('/login', userlogin);
 
 router.get('/products', getProducts);
 router.get('/product/:id', getProductById);
+
+router.post('/payment', addPaymentGateway);
+
 export default router;
