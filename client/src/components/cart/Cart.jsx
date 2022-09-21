30d0c485c3a692d95a2304047c4bfe5ad883a8ae
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-09-22 00:25:35 +0530

    completed

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -9,6 +9,8 @@ import { addToCart, removeFromCart } from '../../redux/actions/cartActions';
 import TotalView from './TotalView';
 import EmptyCart from './EmptyCart';
 import CartItem from './CartItem';
+import { payUsingPaytm } from '../../service/api';
+import { post } from '../../utils/Paytm';
 
 const Component = styled(Grid)(({ theme }) => ({
     padding: '30px 135px',
@@ -53,9 +55,9 @@ const Cart = () => {
     const { id } = useParams();
 
     const dispatch = useDispatch();
-    
+
     useEffect(() => {
-        if(cartItems && id !== cartItems.id)   
+        if (cartItems && id !== cartItems.id)
             dispatch(addToCart(id));
         console.log(cartItems);
     }, [dispatch, cartItems, id]);
@@ -65,30 +67,36 @@ const Cart = () => {
     }
 
     const buyNow = async () => {
-       
+        let response = await payUsingPaytm({ amount: 50, email: 'chahar.krishnaveer36@gmail.com' });
+        let information = {
+            action: 'https://securegw-stage.paytm.in/order/process',
+            params: response
+        }
+        // pass user information to build the form.
+        post(information);
     }
 
     return (
         <>
-        { cartItems.length ? 
-            <Component container>
-                <LeftComponent item lg={9} md={9} sm={12} xs={12}>
-                    <Header>
-                        <Typography style={{fontWeight: 600, fontSize: 18}}>My Cart ({cartItems?.length})</Typography>
-                    </Header>
-                        {   cartItems.map(item => (
-                                <CartItem item={item} removeItemFromCart={removeItemFromCart}/>
-                            ))
+            {cartItems.length ?
+                <Component container>
+                    <LeftComponent item lg={9} md={9} sm={12} xs={12}>
+                        <Header>
+                            <Typography style={{ fontWeight: 600, fontSize: 18 }}>My Cart ({cartItems?.length})</Typography>
+                        </Header>
+                        {cartItems.map(item => (
+                            <CartItem item={item} removeItemFromCart={removeItemFromCart} />
+                        ))
                         }
-                    <BottomWrapper>
-                        <StyledButton onClick={() => buyNow()} variant="contained">Place Order</StyledButton>
-                    </BottomWrapper>
-                </LeftComponent>
-                <Grid item lg={3} md={3} sm={12} xs={12}>
-                    <TotalView cartItems={cartItems} />
-                </Grid>
-            </Component> : <EmptyCart />
-        }
+                        <BottomWrapper>
+                            <StyledButton onClick={() => buyNow()} variant="contained">Place Order</StyledButton>
+                        </BottomWrapper>
+                    </LeftComponent>
+                    <Grid item lg={3} md={3} sm={12} xs={12}>
+                        <TotalView cartItems={cartItems} />
+                    </Grid>
+                </Component> : <EmptyCart />
+            }
         </>
 
     )
--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ import DefaultData from "./default.js";
 import router from "./routes/route.js";
 import cors from "cors";
 import bodyParser from "body-parser";
-import {v4 as uuid} from 'uuid';
-
+import { v4 as uuid } from 'uuid';
+import {useSelector, useDispatch} from 'react-redux';
 const app = express();
 
 dotenv.config();
@@ -31,12 +31,13 @@ DefaultData();
 export let paytmMerchantKey = process.env.PAYTM_MERCHANT_KEY;
 export let paytmParams = {};
 paytmParams['MID'] = process.env.PAYTM_MID,
-paytmParams['WEBSITE'] = process.env.PAYTM_WEBSITE,
-paytmParams['CHANNEL_ID'] = process.env.PAYTM_CHANNEL_ID,
-paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID,
-paytmParams['ORDER_ID'] = uuid();
+  paytmParams['WEBSITE'] = process.env.PAYTM_WEBSITE,
+  paytmParams['CHANNEL_ID'] = process.env.PAYTM_CHANNEL_ID,
+  paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID,
+  paytmParams['ORDER_ID'] = uuid();
 paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID,
-paytmParams['TXN_AMOUNT'] = '100',
-paytmParams['CALLBACK_URL'] = 'http://localhost:8000/callback'
+  paytmParams['TXN_AMOUNT'] = '800',
+  paytmParams['CALLBACK_URL'] = 'http://localhost:8000/callback'
 paytmParams['EMAIL'] = 'chahar.krishnaveer36@gmail.com'
 paytmParams['MOBILE_NO'] = '1234567852'
+
