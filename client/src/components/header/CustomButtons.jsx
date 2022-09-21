1ab29390c50e8ec62b4b1a8dd6d962d55acdb12a
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-09-21 16:39:25 +0530

    add badge for cart

--- client/src/components/header/CustomButtons.jsx
+++ client/src/components/header/CustomButtons.jsx
@@ -1,15 +1,19 @@
-import { Box, Button, Typography, styled } from "@mui/material";
-import { ShoppingCart } from "@mui/icons-material";
-import LoginDialog from "../login/LoginDialog";
-import { useState, useContext } from "react";
-import { DataContext } from "../../context/DataProvider";
-import Profile from "./Profile";
+import React, { useState, useContext } from 'react';
+
+import { Box, Typography, Badge, Button, styled } from '@mui/material';
+import { ShoppingCart } from '@mui/icons-material';
+
 import { Link } from 'react-router-dom';
+import { DataContext } from '../../context/DataProvider';
+import { useSelector } from 'react-redux';
+
+import Profile from './Profile';
+import LoginDialog from '../login/LoginDialog';
 
 const Container = styled(Link)(({ theme }) => ({
   display: 'flex',
   [theme.breakpoints.down('sm')]: {
-      display: 'block'
+    display: 'block'
   }
 }));
 
@@ -17,21 +21,21 @@ const Wrapper = styled(Box)(({ theme }) => ({
   margin: '0 3% 0 auto',
   display: 'flex',
   '& > *': {
-      marginRight: '40px !important',
-      textDecoration: 'none',
-      color: '#FFFFFF',
-      fontSize: 12,
+    marginRight: '40px !important',
+    textDecoration: 'none',
+    color: '#FFFFFF',
+    fontSize: 12,
+    alignItems: 'center',
+    [theme.breakpoints.down('sm')]: {
+      color: '#2874f0',
       alignItems: 'center',
-      [theme.breakpoints.down('sm')]: {
-          color: '#2874f0',
-          alignItems: 'center',
-          display: 'flex',
-          flexDirection: 'column',
-          marginTop: 10
-      }
+      display: 'flex',
+      flexDirection: 'column',
+      marginTop: 10
+    }
   },
   [theme.breakpoints.down('sm')]: {
-      display: 'block'
+    display: 'block'
   }
 }));
 
@@ -45,38 +49,43 @@ const LoginButton = styled(Button)(({ theme }) => ({
   height: 32,
   boxShadow: 'none',
   [theme.breakpoints.down('sm')]: {
-      background: '#2874f0',
-      color: '#FFFFFF'
+    background: '#2874f0',
+    color: '#FFFFFF'
   }
 }));
 
+
 const CustomButtons = () => {
+
   const [open, setOpen] = useState(false);
   const { account, setAccount } = useContext(DataContext);
 
+  const cartDetails = useSelector(state => state.cart);
+  const { cartItems } = cartDetails;
+
   const openDialog = () => {
     setOpen(true);
-  };
+  }
+
   return (
     <Wrapper>
-      {account ? (
-        <Profile account={account} setAccount={setAccount} />
-      ) : (
-        <LoginButton variant="contained" onClick={() => openDialog()}>
-          Login
-        </LoginButton>
-      )}
-      <Typography style={{ marginTop: 3, width: 135 }}>
-        Become a Seller
-      </Typography>
+      {
+        account ? <Profile account={account} setAccount={setAccount} /> :
+          <LoginButton variant="contained" onClick={() => openDialog()}>Login</LoginButton>
+
+      }
+      <Typography style={{ marginTop: 3, width: 135 }}>Become a Seller</Typography>
       <Typography style={{ marginTop: 3 }}>More</Typography>
-      <Container>
-        <ShoppingCart />
-        <Typography>Cart</Typography>
+
+      <Container to='/cart'>
+        <Badge badgeContent={cartItems?.length} color="secondary">
+          <ShoppingCart />
+        </Badge>
+        <Typography style={{ marginLeft: 10 }}>Cart</Typography>
       </Container>
-      <LoginDialog open={open} setOpen={setOpen} />
+      <LoginDialog open={open} setOpen={setOpen} setAccount={setAccount} />
     </Wrapper>
-  );
-};
+  )
+}
 
 export default CustomButtons;
